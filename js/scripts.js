e55c213575a70475c5861df9a78e26d2f93b6da2
ElmerG <gonzagaelmer0597@gmail.com>
2025-04-04 13:18:15 +0800

    major update

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,31 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+// Resume - start
+showAndroidExp();
+
+function showAndroidExp() {
+    // start date (April 2022)
+    const startDate = new Date(2022, 3);
+
+    // Get the current date
+    const currentDate = new Date();
+
+    // Calculate year and month difference
+    let yearDiff = currentDate.getFullYear() - startDate.getFullYear();
+    let monthDiff = currentDate.getMonth() - startDate.getMonth();
+
+    // Adjust if month difference is negative
+    if (monthDiff < 0) {
+        yearDiff--;
+        monthDiff += 12;
+    }
+
+    // Display the result
+    document.getElementById("exp-year-month").textContent = `${yearDiff} years ${ (monthDiff > 0) ? ` and ${monthDiff} months` : ``}`;
+}
+// Resume - end
+
 showCreditsYear();
 
 function showCreditsYear() {
